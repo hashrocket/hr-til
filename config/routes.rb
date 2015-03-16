d5d2776b6379d67b4c363fa1790a6ae5e260f584
Josh Branchaud and Jake Worth <jake+jbranchaud+jwworth@hashrocket.com>
2015-03-16 16:57:47 -0500

    Add developers resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
 
+  resources :developers, only: [:new, :create]
   get 'account/signup' => 'developers#new'
   post 'account/signup' => 'developers#create'
 end
