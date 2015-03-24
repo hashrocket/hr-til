30cae54727cad69024afd50239962666fbfe6cb3
Jake Worth <dev+jwworth@hashrocket.com>
2015-03-24 10:27:54 -0500

    Include tags when loading developer's posts
    [#90310590]

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -13,8 +13,7 @@ class DevelopersController < ApplicationController
 
   def show
     @developer = Developer.find_by_username(params[:username])
-    posts = @developer.posts.order created_at: :desc
-    @post_days = posts.group_by { |p| p.created_at.beginning_of_day }
+    @post_days = @developer.posts(created_at: :desc).includes(:tag).group_by { |p| p.created_at.beginning_of_day }
   end
 
   private
