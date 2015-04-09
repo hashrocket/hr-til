85713c9d6d752877cdcb11d1070490455495e7e9
Jake Worth <dev+jwworth@hashrocket.com>
2015-04-09 11:42:44 -0500

    Don't validate username format
    Username is being generated by Google name. We should be looser
    about this because Google will have its own restrictions on those
    fields and we should respect them until we have a reason not to.

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,5 +1,5 @@
 class Developer < ActiveRecord::Base
   has_many :posts
   validates :email, presence: true
-  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :username, presence: true, uniqueness: true
 end
--- spec/models/developer_spec.rb
+++ spec/models/developer_spec.rb
@@ -16,14 +16,10 @@ describe Developer do
     expect(developer).to_not be_valid
   end
 
-  it 'should validate username format' do
-    developer = FactoryGirl.build(:developer, username: 'foo_bar')
-    expect(developer).to_not be_valid
-  end
-
-  it 'should validate username presence' do
-    developer = FactoryGirl.build(:developer, username: 'foo_bar')
+  it 'should validate username uniqueness' do
+    developer = FactoryGirl.create(:developer, username: 'foo_bar')
     dup_developer = FactoryGirl.build(:developer, username: 'foo_bar')
+
     expect(dup_developer).to_not be_valid
   end
 end
