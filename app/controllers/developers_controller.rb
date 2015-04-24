81dc3676e2ab81ff4d07d2ffe8f001c17579da31
Jake Worth <dev+jwworth@hashrocket.com>
2015-04-24 13:45:59 -0500

    Remove a method that never gets called
    Developer creation and routing is now handled by the sessions
    controller.

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,16 +1,5 @@
 class DevelopersController < ApplicationController
 
-  def create
-    @developer = Developer.new(developer_params)
-    if @developer.save
-      sign_in @developer
-      redirect_to root_path
-    else
-      flash[:alert] = @developer.errors.full_messages
-      render :new
-    end
-  end
-
   def show
     @developer = Developer.find_by_username(params[:username]).decorate
     @post_days = @developer.posts(created_at: :desc).includes(:tag).group_by { |p| p.created_at.beginning_of_day }
