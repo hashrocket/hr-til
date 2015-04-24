5e85d5a9545b0e447b22fad910c61f529742e7c6
Jake Worth <dev+jwworth@hashrocket.com>
2015-04-24 11:14:13 -0500

    Turn on a bunch of redcarpet markdown options
    This adds support for ruby-style underscored variables, strikethrough,
    commented code, underlining, highlighting, and quoting.

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -3,7 +3,13 @@ module MarkdownHelper
     @markdown ||= Redcarpet::Markdown.new(
       Redcarpet::Render::HTML,
       autolink: true,
-      fenced_code_blocks: true
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      strikethrough: true,
+      space_after_headers: true,
+      underline: true,
+      highlight: true,
+      quote: true
     )
   end
 
