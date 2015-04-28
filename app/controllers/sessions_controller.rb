73984ac27ab26204e609048bfa89b046d11a1e7a
Brian Dunn and Jake Worth <dev+briandunn+jwworth@hashrocket.com>
2015-04-28 10:54:06 -0500

    Simplify action with fancy find_or_create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,12 @@ class SessionsController < ApplicationController
 
   def create
     oauth_info = OAuthInfo.new(request.env['omniauth.auth'])
-    if developer = Developer.find_by_email(oauth_info.email)
-      sign_in developer
-      flash[:notice] = 'Signed in'
-      redirect_to root_path
-    else
-      developer = Developer.create(username: oauth_info.username, email: oauth_info.email)
-      sign_in developer
-      flash[:notice] = 'Signed in'
-      redirect_to root_path
+    developer = Developer.find_or_create_by(email: oauth_info.email) do |new_developer|
+      new_developer.username = oauth_info.username
     end
+    sign_in developer
+    flash[:notice] = 'Signed in'
+    redirect_to root_path
   end
 
   def destroy
