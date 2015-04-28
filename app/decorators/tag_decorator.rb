a8d68112203f3df7133feef65d8f5d65b7d7abce
Brian Dunn and Jake Worth <dev+briandunn+jwworth@hashrocket.com>
2015-04-28 10:01:36 -0500

    Reuse post presentation stuffs

--- app/decorators/tag_decorator.rb
+++ app/decorators/tag_decorator.rb
@@ -1,12 +1,4 @@
 class TagDecorator < Draper::Decorator
+  include PresentsPosts
   delegate_all
-
-  def posts_count
-    count = posts.count
-    if count > 1
-      "(#{count} posts)"
-    elsif count == 1
-      "(#{count} post)"
-    end
-  end
 end
