f916fd9614e29b8db8d1948697f94012f0198700
Jake Worth <dev+jwworth@hashrocket.com>
2015-04-28 13:38:44 -0500

    Refactor developer spec to reuse common object

--- spec/models/developer_spec.rb
+++ spec/models/developer_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
 describe Developer do
+  let(:developer) { FactoryGirl.create(:developer) }
+
   it 'should have a valid factory' do
-    developer = FactoryGirl.build(:developer)
     expect(developer).to be_valid
   end
 
   it 'should require a username' do
-    developer = FactoryGirl.build(:developer, username: '')
+    developer.username = ''
     expect(developer).to_not be_valid
   end
 
   it 'should require an email' do
-    developer = FactoryGirl.build(:developer, email: '')
+    developer.email = ''
     expect(developer).to_not be_valid
   end
 
   it 'should validate username uniqueness' do
-    developer = FactoryGirl.create(:developer, username: 'foo_bar')
-    dup_developer = FactoryGirl.build(:developer, username: 'foo_bar')
-
+    dup_developer = FactoryGirl.build(:developer, username: developer.username)
     expect(dup_developer).to_not be_valid
+    expect(dup_developer.errors.messages[:username]).to eq ['has already been taken']
   end
 end
