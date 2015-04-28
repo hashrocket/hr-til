1644a4d044216bd7ebc9200e14689b8d2815d1f8
Brian Dunn and Jake Worth <dev+briandunn+jwworth@hashrocket.com>
2015-04-28 10:17:07 -0500

    Require spec support files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
+require 'pathname'
+
+Dir[Pathname(__FILE__).dirname.join('support/*.rb')].each { |file| require file }
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
