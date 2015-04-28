2cc6e473ffc873609e03c524c7b1d56844a0e109
Brian Dunn and Jake Worth <dev+briandunn+jwworth@hashrocket.com>
2015-04-28 11:01:40 -0500

    Display page not found when tag does not exist
    [#93450944]

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,6 @@
 class TagsController < ApplicationController
-
   def show
-    @tag = Tag.find_by_name(params[:name]).decorate
+    @tag = Tag.find_by_name!(params[:name]).decorate
     posts = @tag.posts.order created_at: :desc
     @post_days = posts.group_by { |p| p.created_at.beginning_of_day }
   end
