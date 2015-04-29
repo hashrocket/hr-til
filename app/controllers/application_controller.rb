2df384528d48ffe8efd917507061face5d46728e
Jack Christensen and Jake Worth <dev+jackc+jwworth@hashrocket.com>
2015-04-29 14:20:44 -0500

    Set cache headers to no-cache, no-store
    [#93474134]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_cache_header
 
   authem_for :developer
 
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
     unless current_developer
       redirect_to root_path
     end
+  end
+
+  def set_cache_header
+    headers['Cache-Control'] = 'no-cache, no-store'
   end
 end
