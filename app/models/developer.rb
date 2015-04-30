ba71c463277614230a7f5dd5aa4a799f06d41ff9
Joshua Davey and Jake Worth <dev+jgdavey+jwworth@hashrocket.com>
2015-04-30 09:41:17 -0500

    Remove leading @ symbols from any twitter handle
    [#93227284]

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -2,4 +2,8 @@ class Developer < ActiveRecord::Base
   has_many :posts
   validates :email, presence: true
   validates :username, presence: true, uniqueness: true
+
+  def twitter_handle=(handle)
+    write_attribute(:twitter_handle, handle.to_s.gsub(/^@/, ''))
+  end
 end
