67ddf2458b01ca9cc817405d8e861db57dcd9213
Jake Worth <dev+jwworth@hashrocket.com>
2015-05-05 09:27:52 -0500

    Cleanup spacing

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,10 +2,10 @@ class StatisticsController < ApplicationController
   before_filter :require_admin
 
   def tag_posts_counts
-    render json: Tag.all.map{|tag| [tag.name, tag.posts.count]}.to_h
+    render json: Tag.all.map { |tag| [tag.name, tag.posts.count] }.to_h
   end
 
   def author_posts_counts
-    render json: Developer.all.map{|developer| [developer.username, developer.posts.count]}.to_h
+    render json: Developer.all.map { |developer| [developer.username, developer.posts.count] }.to_h
   end
 end
