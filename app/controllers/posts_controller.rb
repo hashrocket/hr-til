634e588027b31ac52832f226546670c023761293
Brian Dunn and Jake Worth <dev+briandunn+jwworth@hashrocket.com>
2015-05-18 15:00:19 -0500

    Only allow updates to a post by its author

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
-  before_filter :require_developer, except: [:index, :show]
-  before_filter :authorize_developer, only: [:edit]
+  before_action :require_developer, except: [:index, :show]
+  before_action :authorize_developer, only: [:edit, :update]
 
   def preview
     render layout: false
---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe PostsController do
+  describe '#update' do
+    let(:a_post) { FactoryGirl.create :post }
+    let(:developer) { FactoryGirl.create :developer }
+    before { controller.sign_in developer }
+
+    context 'as a developer' do
+      it 'only allows me to update my own posts' do
+        expect do
+          patch :update, titled_slug: a_post.to_param, post: {title: 'HAXORD'}
+        end.to_not change { a_post.reload.title }
+      end
+    end
+
+    context 'as an admin' do
+      let(:developer) { FactoryGirl.create :developer, admin: true }
+
+      it 'allows me to update anyones post' do
+        expect do
+          patch :update, titled_slug: a_post.to_param, post: {title: 'this is ok'}
+        end.to change { a_post.reload.title }
+      end
+    end
+  end
+end
