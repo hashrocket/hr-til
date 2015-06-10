df23b9d14be61460fcd87c712318c667cf58ced8
Jake Worth <dev+jwworth@hashrocket.com>
2015-06-09 19:43:39 -0500

    Refactor for DRY and type comparisons

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
+  var slug, liked, like_text
   var $likeActionEl = $(".js-like-action");
 
   $likeActionEl.each(function(index, link) {
-    var slug = link.id;
-    var liked = $.cookie('liked-' + slug);
+    slug = link.id;
+    liked = $.cookie(likeSlug(slug));
     if (liked) {
       $('#' + slug).addClass('liked');
     }
@@ -11,20 +12,27 @@ $(document).ready(function() {
 
   $likeActionEl.click(function(e) {
     e.preventDefault();
-    var slug = this.id;
-    var liked = $.cookie('liked-' + slug);
-    if (liked) {
-      $.post("/posts/" + slug + "/unlike.json", function(result){
-        var like_text = result.likes == 1 ? " time" : " times";
-        $('#' + slug).removeClass('liked').html('liked <b>' + result.likes + like_text + '</b>');
-        $.removeCookie('liked-' + slug, { path: '/', expires: 3600 });
+    slug = this.id;
+    liked = $.cookie(likeSlug(slug));
+    if (liked === undefined) {
+      $.post("/posts/" + slug + "/like.json", function(result){
+        $('#' + slug).addClass('liked').html(newHtml(result));
+        $.cookie(likeSlug(slug), 'liked', { path: '/', expires: 3600 });
       });
     } else {
-      $.post("/posts/" + slug + "/like.json", function(result){
-        var like_text = result.likes == 1 ? " time" : " times";
-        $('#' + slug).addClass('liked').html('liked <b>' + result.likes + like_text + '</b>');
-        $.cookie('liked-' + slug, 'liked', { path: '/', expires: 3600 });
+      $.post("/posts/" + slug + "/unlike.json", function(result){
+        $('#' + slug).removeClass('liked').html(newHtml(result));
+        $.removeCookie(likeSlug(slug), { path: '/', expires: 3600 });
       });
     }
   });
+
+  function newHtml(result) {
+    like_text = result.likes === 1 ? " time" : " times";
+    return 'liked <b>' + result.likes + like_text + '</b>';
+  }
+
+  function likeSlug(slug) {
+    return 'liked-' + slug;
+  }
 });
