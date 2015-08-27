296f39a7c73609c82efcb0de61a19c48bb308c5d
Chris Erin <dev+chriserin@hashrocket.com>
2015-08-27 14:21:38 -0500

    Add visual tag for draft

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(*Rails.groups)
 
 module HrTil
   class Application < Rails::Application
-    config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('lib', 'app/decorators')
     config.time_zone = 'Eastern Time (US & Canada)'
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
