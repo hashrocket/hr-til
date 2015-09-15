8d2e53dccc08c3483d6e68ba2fc8b91e8ff92a83
Cameron Daigle <camerondaigle@gmail.com>
2015-09-15 11:34:35 -0400

    Refactor like.js

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -1,38 +1,54 @@
-$(document).ready(function() {
-  var slug, liked, like_text
-  var $likeActionEl = $(".js-like-action");
+$(function(){
 
-  $likeActionEl.each(function(index, link) {
-    slug = link.id;
-    liked = $.cookie(likeSlug(slug));
-    if (liked) {
-      $('#' + slug).addClass('liked');
-    }
-  });
+  function LikeButton(el) {
+    this.id = el.id;
+    this.$el = $(el);
+    this.$el.on("click", $.proxy(this.toggle, this));
+    this.updateClass();
+  };
 
-  $likeActionEl.click(function(e) {
+  LikeButton.prototype.toggle = function(e) {
     e.preventDefault();
-    slug = this.id;
-    liked = $.cookie(likeSlug(slug));
-    if (liked === undefined) {
-      $.post("/posts/" + slug + "/like.json", function(result){
-        $('#' + slug).addClass('liked').html(newHtml(result));
-        $.cookie(likeSlug(slug), 'liked', { path: '/', expires: 3600 });
-      });
-    } else {
-      $.post("/posts/" + slug + "/unlike.json", function(result){
-        $('#' + slug).removeClass('liked').html(newHtml(result));
-        $.removeCookie(likeSlug(slug), { path: '/', expires: 3600 });
-      });
-    }
+    this.isLiked() ? this.unlike() : this.like();
+  };
+
+  LikeButton.prototype.like = function() {
+    var lb = this;
+    $.post("/posts/" + lb.id + "/like.json", function(result){
+      $.cookie(lb.likeSlug(), 'liked', { path: '/', expires: 3600 });
+      lb.updateText(result);
+      lb.updateClass();
+    });
+  };
+
+  LikeButton.prototype.unlike = function() {
+    var lb = this;
+    $.post("/posts/" + lb.id + "/unlike.json", function(result){
+      $.removeCookie(lb.likeSlug(), { path: '/', expires: 3600 });
+      lb.updateText(result);
+      lb.updateClass();
+    });
+  };
+
+  LikeButton.prototype.updateClass = function() {
+    this.$el.toggleClass('liked', this.isLiked());
+  }
+
+  LikeButton.prototype.updateText = function(result) {
+    like_text = result.likes === 1 ? " time" : " times";
+    this.$el.html('liked <b>' + result.likes + like_text + '</b>');
+  };
+
+  LikeButton.prototype.isLiked = function() {
+    return !!$.cookie(this.likeSlug());
+  };
+
+  LikeButton.prototype.likeSlug = function() {
+    return 'liked -' + this.id;
+  };
+
+  $('.js-like-action').each(function() {
+    new LikeButton(this);
   });
 
-  function newHtml(result) {
-    like_text = result.likes === 1 ? " time" : " times";
-    return 'liked <b>' + result.likes + like_text + '</b>';
-  }
-
-  function likeSlug(slug) {
-    return 'liked-' + slug;
-  }
 });
