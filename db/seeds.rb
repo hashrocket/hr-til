441caa13e8cf6f7e29c653a972489a4cabdac37f
Jake Worth <dev+jwworth@hashrocket.com>
2015-10-12 20:41:45 -0500

    Fix typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 channels = %w(vim development design consulting rails ruby testing bash git
-              html/css javascript clojure sql mobile devops go internet elixir emberjs)
+              html-css javascript clojure sql mobile devops go internet elixir emberjs)
 
 channels.each do |channel|
   puts "Finding or creating channel: #{channel}"
