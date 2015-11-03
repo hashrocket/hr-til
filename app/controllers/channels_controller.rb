bd7f6247823915bfdf387b444aa1bd123a63fdb6
Dorian Karter and Jake Worth <dev+dkarter+jwworth@hashrocket.com>
2015-11-03 16:09:27 -0600

    Convert channels controller to helper methods

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,6 +1,13 @@
 class ChannelsController < ApplicationController
-  def show
-    @channel = Channel.find_by_name!(params[:id])
-    @posts = @channel.posts.published_and_ordered.includes(:developer)
+  helper_method :channel, :posts
+
+  private
+
+  def channel
+    @channel ||= Channel.find_by_name!(params[:id])
+  end
+
+  def posts
+    @posts ||= channel.posts.published_and_ordered.includes(:developer)
   end
 end
