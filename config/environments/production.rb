1826bb74461a2de03ea7b88c0d636e577dd198b1
Clemens Park <clemensp@nulogy.com>
2016-02-05 14:16:11 -0500

    gets the host from the config file instead of hardcoding it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_controller.default_url_options = {
-    host: 'til.hashrocket.com',
+    host: ENV['host'],
     protocol: 'https://'
   }
 end
