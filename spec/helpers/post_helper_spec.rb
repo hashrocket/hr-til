7d6814fc5df7e8593ad7eef1813342e70a83237a
Clemens Park <clemensp@nulogy.com>
2016-02-05 15:09:07 -0500

    decouples the twitter spec from the application config

--- spec/helpers/post_helper_spec.rb
+++ spec/helpers/post_helper_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 describe PostHelper do
   describe '#tweet_link' do
     it 'returns a link to twitter' do
+      stub_const('ENV', { 'default_twitter_handle' => 'twitter_handle' })
+
       @post = FactoryGirl.create(:post)
       @post.slug = '1234'
       @post.save!
 
-      expected_result = "<a href=\"http://twitter.com/share\" class=\"twitter-share-button\" data-text=\"Today I learned: Web Development\" data-via=\"hashrocket\" data-hashtags=\"phantomjs\" data-url=\"http://www.example.com/posts/1234-web-development\">Tweet</a>"
+      expected_result = "<a href=\"http://twitter.com/share\" class=\"twitter-share-button\" data-text=\"Today I learned: Web Development\" data-via=\"twitter_handle\" data-hashtags=\"phantomjs\" data-url=\"http://www.example.com/posts/1234-web-development\">Tweet</a>"
 
       expect(helper.tweet_link(@post)).to eq expected_result
     end
