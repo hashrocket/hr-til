d07f5f7baafa8c04b4cae173ff65f5eb66c6e250
Jake Worth <jakeworth82@gmail.com>
2016-02-21 18:34:03 -0600

    Add max likes to development seeds

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -10,12 +10,14 @@ end
 40.times do |i|
   puts "Creating post ##{i + 1}"
   channel = Channel.all.sample
+  likes = rand(1..20)
 
   Post.create!(body: Phil.words(100).capitalize,
     channel: channel,
     developer: Developer.all.sample,
     title: Phil.words(2).capitalize,
-    likes: rand(1..10),
+    likes: likes,
+    max_likes: likes,
     created_at: Date.today - rand(30).days,
     published_at: [(Date.today - rand(30).days), nil].sample)
 end
