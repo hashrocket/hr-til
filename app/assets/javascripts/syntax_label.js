92a76b6e1f60e1db309daa2aed10dee1983c4270
Cameron Daigle <camerondaigle@gmail.com>
2016-06-02 16:13:30 -0400

    Only happens when people are watching

--- app/assets/javascripts/syntax_label.js
+++ app/assets/javascripts/syntax_label.js
@@ -5,7 +5,6 @@ $(function() {
     var $hl = $el.filter('.highlight').add($el.find('.highlight'));
     $hl.each(function() {
       var language = $(this).attr('class').replace(/\s*highlight\s*/, '');
-      console.log(language);
       $(this).attr('data-language', language);
     });
   }
