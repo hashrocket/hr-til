c1a0fda39cb505cce73bc3556e410fd447e273a7
Obie Fernandez <obie@andela.co>
2016-10-06 15:25:18 -0400

    turn off force ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
-  config.force_ssl = true
+  config.force_ssl = false
 
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
