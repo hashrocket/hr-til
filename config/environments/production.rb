8a6353ac4cdb8df36b27f8fa448d78432710667c
Obie Fernandez <obie@andela.co>
2016-10-06 17:15:25 -0400

    turn on serving assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
   config.assets.js_compressor = :uglifier
 
   config.assets.compile = false
