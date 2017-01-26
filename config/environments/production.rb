31631abb0bb8d0fa9f9b9a9721f3a179b1f6dffb
Tim Gremore <tim@headway.io>
2017-01-25 21:35:17 -0600

    Disable asset pipeline environment settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -45,7 +45,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.assets.digest = true
+  config.assets.digest = false
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
